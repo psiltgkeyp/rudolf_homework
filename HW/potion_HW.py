7fbbe32b9098570c0089d1a392c6e357a0ad1d21
psiltgkeyp <bapepusi@yandex.ru>
2026-02-05 21:15:11 +0300

    Update potion_HW.py

--- HW/potion_HW.py
+++ HW/potion_HW.py
@@ -1,1 +1,8 @@
-
+class Potion:
+    def __init__(self, name, power):
+        self.name = name
+        self.power = power
+    def drink(self):
+        return f"Ты выпил {self.name}! Сила: {self.power}"
+hp = Potion("Зелька хила", 50)
+print(hp.drink())
